9ce8cb13a56d97a5943d7cf88a3d745655c35d50
captproton <carl@wdwhub.net>
2024-08-08 17:06:15 -0700

    ADD: decorator for BlogController because Zeitwerk::NameError

---
+++ app/decorators/controllers/blog_controller_decorator.rb
@@ -0,0 +1,7 @@
+module Controllers
+  class BlogControllerDecorator
+    # This is a decorator for the BlogController
+    # The main app wants this, but we don't need it anymore
+    # Maybe we can remove it in the future
+  end
+end
