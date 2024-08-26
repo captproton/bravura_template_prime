4b75353b62bc35e4a3d77edb940c557f456d1f23
captproton <carl@wdwhub.net>
2024-08-26 15:37:17 -0700

    FIX: attempt to fix staging

--- app/assets/config/bravura_template_prime_manifest.js
+++ app/assets/config/bravura_template_prime_manifest.js
@@ -4,4 +4,4 @@
 //= link_directory ../images .gif
 //= link_directory ../images .svg
 //= link_directory ../stylesheets/bravura_template_prime .css
-//= link_directory ../../javascript/bravura_template_prime .js
+//= link_directory ../../javascript .js
