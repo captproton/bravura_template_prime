f80a15e288d6baef7cb6629e6dea594ae073105a
captproton <carl@wdwhub.net>
2024-08-26 08:45:25 -0700

    FIX: path for default_avatar was wrong on a show page

--- app/helpers/bravura_template_prime/authors_helper.rb
+++ app/helpers/bravura_template_prime/authors_helper.rb
@@ -5,7 +5,7 @@ module BravuraTemplatePrime
       if author.avatar.present?
         url_for(author.avatar.variant(resize: "#{size}x#{size}"))
       else
-        asset_path("default_avatar.jpg")
+        asset_path("bravura_template_prime/default_avatar.jpg")
       end
     end
   end
