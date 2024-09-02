5692f444e525e54141135645072fecf680848a1b
captproton <carl@wdwhub.net>
2024-09-02 15:10:38 -0700

    CHANGE: made button radius and navbar position editable

--- app/controllers/bravura_template_prime/application_controller.rb
+++ app/controllers/bravura_template_prime/application_controller.rb
@@ -1,4 +1,14 @@
 module BravuraTemplatePrime
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
+    include BravuraTemplateBase::BaseControllerConcern
+    helper BravuraTemplatePrime::ApplicationHelper
+    include BravuraTemplatePrime::ApplicationHelper
+
+    before_action :load_settings_and_presenter
+
+    def cta_button_radius
+      "0.5rem"
+    end
   end
+
 end
