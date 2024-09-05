a3ab0e054462f3908c3b82e152d9de44ca388266
captproton <carl@wdwhub.net>
2024-09-05 12:48:29 -0700

    CHANGE: s/@presenter/@gss/ temporarily since presenter is broken

--- app/presenters/bravura_template_prime/presenter.rb
+++ app/presenters/bravura_template_prime/presenter.rb
@@ -1,10 +1,11 @@
 # app/presenters/bravura_template_prime/presenter.rb
 module BravuraTemplatePrime
   class Presenter < BravuraTemplateBase::BasePresenter
-    def get(key)
-      @settings.get(key)
-    end
-
-    # Other Prime-specific methods...
+  #   def get(key)
+  #     value = super(key)
+  #     # Add any Prime-specific logic here if needed
+  #     value
+  #   end
+  #   # Other Prime-specific methods...
   end
 end
